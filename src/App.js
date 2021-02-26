045a8ab241ee110aead83007b5260cbde3e5e7e6
Lindell Carter <lindellcarternyc@gmail.com>
2021-02-26 10:36:40 -0500

    setup router and add initial routes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,36 @@
 import React from 'react'
+import { Switch, Route } from 'react-router-dom'
 
+// LAYOUT COMPONENTS
 import Header from './components/Header'
 import Main from './components/Main'
 import Footer from './components/Footer'
+
+// PAGES
+import Home from './pages/Home'
+import NotFoud from './pages/404'
+import Confirm from './pages/Confirm'
+import Pizza from './pages/Pizza'
 
 const App = () => {
   return (
     <>
       <Header />
       <Main>
-        Hey Girl!
+        <Switch>
+          <Route path="/confirm">
+            <Confirm />
+          </Route>
+          <Route path="/pizza">
+            <Pizza />
+          </Route>
+          <Route path="/" exact>
+            <Home />
+          </Route>
+          <Route>
+            <NotFoud />
+          </Route>
+        </Switch>
       </Main>
       <Footer />
     </>
---
+++ src/pages/Confirm.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Confirm(props) {
+    return (
+        <div>
+            Confirm
+        </div>
+    )
+}
