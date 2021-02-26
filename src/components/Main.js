cb9d71040a73718ac267ccd1f7f54421dc87f341
Lindell Carter <lindellcarternyc@gmail.com>
2021-02-26 10:18:15 -0500

    add main component

---
+++ src/components/Main.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const MainWrapper = styled.main`
+    padding-top: 70px;
+    padding-bottom: 60px;
+`
+
+export default function Main(props) {
+    const { children } = props
+
+    return (
+        <MainWrapper>
+            {children}
+        </MainWrapper>
+    )
+}
